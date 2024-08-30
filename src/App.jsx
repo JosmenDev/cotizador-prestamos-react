b08801a9b69a9e678af806a7944a2a987d9b3de5
Jose A. Menacho Minchola <josmendev@gmail.com>
2024-08-30 16:02:56 -0500

    Botones para incremento

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ function App() {
   // Dentro de la funcion useState(valorInicial) se coloca el valor inicial
   const [cantidad, setCantidad] = useState(10000);
 
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
   // console.log(cantidad);
   // cantidad = 10000
 
@@ -21,6 +25,24 @@ function App() {
     // Con el + se convierte a un valor numerico
     setCantidad(+e.target.value);
     // tengo que asignarle a setCantidad porque este recibe el valor y modifica la cantidad, no se puede modificar directamente.
+  }
+
+  const handleClickDecremento = e => {
+    const valor = cantidad - STEP;
+    if (valor<MIN) {
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
+  }
+
+  const handleClickIncremento = e => {
+    const valor = cantidad + STEP;
+    if (valor>MAX) {
+      alert('Cantidad no válida');
+      return;
+    }
+    setCantidad(valor);
   }
 
   // aqui puedo crear funciones, variables, etc, antes del return
@@ -33,12 +55,29 @@ function App() {
     /* cuando se coloca llaves, JSX lo a va a tratar como codigo de JS */
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
+      <div className='flex justify-between my-6'>
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleClickDecremento}
+        >-</button>
+        <button
+          type='button'
+          className='h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500'
+          onClick={handleClickIncremento}
+        >+</button>
+
+      </div>
       <input 
         type="range" 
         name="" 
         id="" 
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
       />
       <p className="text-center  my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
     </div>  
