0f5276ee64b3d74a335a236b0804c8ed28ec121b
Jose A. Menacho Minchola <josmendev@gmail.com>
2024-08-30 15:10:26 -0500

    Manejo de State

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import {useState} from 'react';
+// se usa antes del return
 import Header from "./components/Header";
 
 function App() {
+
+  // Tengo que definir el uso de mis hooks fuera del return
+  // no se puede colocar la definicion de hook dentro de un if
+  // aqui definimos las variables y tenemos un arreglo, es destructuring de un arreglo, el useState nos retorna un arreglo 
+  // Primero se le asigna el nombre que deseamos al state
+  // Segundo se le asigna la funcion que va a modificar el state, la recomendacion es colocar set seguido del nombre del state
+  // Dentro de la funcion useState(valorInicial) se coloca el valor inicial
+  const [cantidad, setCantidad] = useState(10000);
+
+  // console.log(cantidad);
+  // cantidad = 10000
+
+  // defino una funcion
+
+  const handleChange = e => {
+    // Con el + se convierte a un valor numerico
+    setCantidad(+e.target.value);
+    // tengo que asignarle a setCantidad porque este recibe el valor y modifica la cantidad, no se puede modificar directamente.
+  }
+
   // aqui puedo crear funciones, variables, etc, antes del return
   // en el return iran los template o codigo html
   // Estos componentes de React tiene  dos partes
@@ -11,7 +33,15 @@ function App() {
     /* cuando se coloca llaves, JSX lo a va a tratar como codigo de JS */
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
-    </div>
+      <input 
+        type="range" 
+        name="" 
+        id="" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+      />
+      <p className="text-center  my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
+    </div>  
   );
 }
 
